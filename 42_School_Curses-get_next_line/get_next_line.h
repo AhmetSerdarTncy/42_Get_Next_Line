ebaaba9fad85d7ae248e240e18fba77dcf1e8b8b
Ahmet Serdar TUNÇAY <119676106+AhmetSerdarTncy@users.noreply.github.com>
2023-09-05 17:12:36 +0300

    Add files via upload

---
+++ 42_School_Curses-get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahtuncay <ahtuncay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 16:12:48 by ahtuncay          #+#    #+#             */
+/*   Updated: 2023/09/05 16:16:35 by ahtuncay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 5
+#endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_free_stash(char **stash, int create_line);
+
+#endif
